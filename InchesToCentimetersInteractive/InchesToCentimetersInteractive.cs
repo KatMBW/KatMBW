f8668cabe950f6264830795817a92274695e281f
Kat <katlynmbwilliamson@gmail.com>
2025-05-27 17:25:55 -0400

    Add files via upload

---
+++ InchesToCentimetersInteractive/InchesToCentimetersInteractive.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace InchesToCentimetersInteractive
+{
+    class InchesToCentimetersInteractive
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter inches you need converted to centimeters >>");
+            var inches = double.Parse(Console.ReadLine());
+            var centimeters = inches * 2.54;
+            Console.WriteLine("{0} inches is equal to {1} centimeters", inches, centimeters);
+            
+        }
+    }
+}
